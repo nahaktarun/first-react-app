b3f6f642524bd3ab267d6bb8db6bf52d734ff7d8
tarun nahak <tarunnahak25@gmail.com>
2022-12-29 07:27:04 +0530

    initial project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,11 +15,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          By Tarun
         </a>
       </header>
     </div>
   );
 }
+
+// git -> staging env -> commiting -> pushing to remote
 
 export default App;
